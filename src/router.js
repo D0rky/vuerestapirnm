89921332d4fe3ab0eb0eff35a2b92ec4691c8ac1
nhinojosa <nhinojosa@southtexascollege.edu>
2023-10-17 07:11:24 -0500

    removed hypensf from router.js and vite.config to match repo

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 
 const routes = [
-  { path: '/vue-restapi-rnm/', name: 'Home', component: HomePage },
+  { path: '/vuerestapirnm/', name: 'Home', component: HomePage },
   {
     path: '/character/:id',
     name: 'Character',
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-restapi-rnm/',
+  base: '/vuerestapirnm/',
   plugins: [vue()],
   resolve: {
     alias: {
